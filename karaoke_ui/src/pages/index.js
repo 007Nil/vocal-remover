dd1ba6fe2cb26c3c1da5a277eb6e9418209377bb
007Nil <sagniksarkar@ymail.com>
2024-01-27 03:00:47 +0530

    deleted extra files

--- karaoke_ui/src/pages/index.js
+++ karaoke_ui/src/pages/index.js
@@ -1,25 +1,89 @@
 import Head from 'next/head';
-import { subDays, subHours } from 'date-fns';
-import { Box, Container, Unstable_Grid2 as Grid } from '@mui/material';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import {
+  Box,
+  Container,
+  Pagination,
+  Stack,
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+import { CompanyCard } from 'src/sections/companies/company-card';
+import { CompaniesSearch } from 'src/sections/companies/youtube-search';
 
-
-const now = new Date();
+const companies = [
+  {
+    id: '2569ce0d517a7f06d3ea1f24',
+    createdAt: '27/03/2019',
+    description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
+    logo: '/assets/logos/logo-dropbox.png',
+    title: 'Dropbox',
+    downloads: '594'
+  }
+];
 
 const Page = () => (
   <>
     <Head>
       <title>
-        Overview | Devias Kit
+        Karaoke Maker
       </title>
     </Head>
-  </>
-);
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="xl">
+        <Stack spacing={3}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            spacing={4}
+          >
+            <Stack spacing={1}>
+              <Stack
+                alignItems="center"
+                direction="row"
+                spacing={1}
+              >
+              </Stack>
+            </Stack>
+            <div>
 
-Page.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
+            </div>
+          </Stack>
+          <CompaniesSearch />
+          <Grid
+            container
+            spacing={3}
+          >
+            {companies.map((company) => (
+              <Grid
+                xs={12}
+                md={6}
+                lg={4}
+                key={company.id}
+              >
+                <CompanyCard company={company} />
+              </Grid>
+            ))}
+          </Grid>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <Pagination
+              count={3}
+              size="small"
+            />
+          </Box>
+        </Stack>
+      </Container>
+    </Box>
+  </>
 );
 
 export default Page;
